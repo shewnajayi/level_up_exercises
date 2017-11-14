3c136fed67090d67261938ae40ad4f43c93bc614
Seun Ajayi <seunfunmilola@gmail.com>
2017-11-14 11:34:14 +0100

    Fixed triangle_facts.rb according to github style guide

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,25 +1,23 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_accessor :side1,:side2,:side3
+	attr_accessor :side1, :side2, :side3
 
-	def initialize(side1,side2,side3)
-		@side1,@side2,@side3 = side1,side2,side3
+	def initialize(side1, side2, side3)
+		@side1 = side1
+		@side2 = side2 
+		@side3 = side3
 	end
 
-	def equilateral()
-		return side1 == side2 && side2 == side3
+	def equilateral
+		return (side1 == side2) && (side2 == side3)
 	end
 
-	def isosceles()
-		return [side1,side2,side3].uniq.length == 2
+	def isosceles
+		return [side1, side2, side3].uniq.length == 2
 	end
 
-	def scalene()
-		unless ! (equilateral || isosceles)
-	    false
-	  else
-	    true
-	  end
+	def scalene
+		return true unless !(equilateral || isosceles)
 	end
 
 	def recite_facts
@@ -27,15 +25,15 @@ class Triangle
 		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
 		puts 'This triangle is scalene and mathematically boring.' if scalene 
 
-		angles = self.calculate_angles(side1,side2,side3)
+		angles = self.calculate_angles(side1, side2, side3)
 		puts 'The angles of this triangle are ' + angles.join(',')
 
 		puts 'This triangle is also a right triangle!' if angles.include? 90
 		puts ''
 	end
 
-	def calculate_angles(a,b,c)
-		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
+	def calculate_angles(a, b, c)
+	  angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
 		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
 		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
 
